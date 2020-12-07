129fcc5efd104f236314488b09196547b74ec55d
Michael Torres <michaeltorresberdugo@gmail.com>
2020-12-06 19:23:27 -0500

    delete corregido, control de libros existentes

--- app/src/main/java/com/example/librosmaterial/CrearLibros.java
+++ app/src/main/java/com/example/librosmaterial/CrearLibros.java
@@ -1,20 +1,31 @@
 package com.example.librosmaterial;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+
+import java.util.ArrayList;
 
 public class CrearLibros extends AppCompatActivity {
 
@@ -22,6 +33,9 @@ public class CrearLibros extends AppCompatActivity {
     private ImageView foto;
     private Uri uri;
     StorageReference storageReference;
+    private DatabaseReference databaseReference;
+    private String bd= "Libros";
+    public boolean existe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,28 +53,27 @@ public class CrearLibros extends AppCompatActivity {
 
 
     public void guardar(View v){
+     validar(v);
+    }
+
+    public void guardarLibro(View v){
         String isbn,tit, aut, id, noPag , edito;
         Libro l ;
         InputMethodManager imp;
-
-        if (validar()) {
-
-            isbn = ISBN.getText().toString();
-            tit = titulo.getText().toString();
-            aut = autor.getText().toString();
-            noPag = noPaginas.getText().toString();
-            edito = editorial.getText().toString();
-            id = Datos.getId();
-            imp = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-            l = new Libro(id, isbn, tit, aut , noPag,edito);
-            l.guardar();
-            limpiar();
-            subir_foto(id);
-            imp.hideSoftInputFromWindow(ISBN.getWindowToken(), 0);
-            Snackbar.make(v, R.string.mensaje_libro_guardado, Snackbar.LENGTH_LONG).show();
-            uri=null;
-        }
-
+        isbn = ISBN.getText().toString();
+        tit = titulo.getText().toString();
+        aut = autor.getText().toString();
+        noPag = noPaginas.getText().toString();
+        edito = editorial.getText().toString();
+        id = Datos.getId();
+        imp = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        l = new Libro(id, isbn, tit, aut , noPag,edito);
+        l.guardar();
+        limpiar();
+        subir_foto(id);
+        imp.hideSoftInputFromWindow(ISBN.getWindowToken(), 0);
+        Snackbar.make(v, R.string.mensaje_libro_guardado, Snackbar.LENGTH_LONG).show();
+        uri=null;
     }
 
     public void subir_foto(String id){
@@ -76,8 +89,9 @@ public class CrearLibros extends AppCompatActivity {
         ISBN.setText("");
         titulo.setText("");
         autor.setText("");
-        noPaginas.requestFocus();
-        editorial.requestFocus();
+        noPaginas.setText("");
+        editorial.setText("");
+        ISBN.requestFocus();
         foto.setImageResource(android.R.drawable.ic_menu_gallery);
     }
 
@@ -105,41 +119,58 @@ public class CrearLibros extends AppCompatActivity {
         }
     }
 
-    public boolean validar(){
+    public void validar(View v){
         if(ISBN.getText().toString().isEmpty()){
             ISBN.setError(getString(R.string.mensaje_error_ISBN));
             ISBN.requestFocus();
-            return false;
         }
 
         if(titulo.getText().toString().isEmpty()){
             titulo.setError(getString(R.string.mensaje_error_titulo));
             titulo.requestFocus();
-            return false;
         }
 
         if(autor.getText().toString().isEmpty()){
             autor.setError(getString(R.string.mensaje_error_autor));
             autor.requestFocus();
-            return false;
+
         }
 
         if(noPaginas.getText().toString().isEmpty()){
             noPaginas.setError(getString(R.string.mensaje_error_no_paginas));
             noPaginas.requestFocus();
-            return false;
         }
 
         if(editorial.getText().toString().isEmpty()){
             editorial.setError(getString(R.string.mensaje_error_editorial));
             editorial.requestFocus();
-            return false;
         }
 
         if (uri == null){
             Snackbar.make((View)ISBN, R.string.mensaje_error_foto, Snackbar.LENGTH_LONG).show();
-            return false;
         }
-        return true;
+        crearLibroDatabase(ISBN , v);
+    }
+
+    private void crearLibroDatabase(EditText ISBN, View v) {
+        ArrayList<Libro> libros = new ArrayList<>();
+        Log.i("ISB" , ISBN.getText().toString());
+        databaseReference= FirebaseDatabase.getInstance().getReference();
+        databaseReference.child(bd).orderByChild("isbn").equalTo(ISBN.getText().toString()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    Snackbar.make((View)ISBN, R.string.mensaje_error_ya_existe, Snackbar.LENGTH_LONG).show();
+                   Log.i("entre" , "entreee");
+                }else{
+                    guardarLibro(v);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
